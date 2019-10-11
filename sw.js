dd2517b118a47debc43bbd2dab08e843f5394fab
mididev <lukas@sansiba.com>
2019-10-11 16:26:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,9 @@ var filesToCache = [
   '/mediaplayer/css/style.css',
   '/mediaplayer/js/main.js',
   '/mediaplayer/images/artists/chaka-khan.jpg',
-  '/mediaplayer/images/artists/nate-smith.jpg'
+  '/mediaplayer/images/artists/nate-smith.jpg',
+  '/mediaplayer/mp3/chaka-khan-a-night-in-tunisia.mp3',
+  '/mediaplayer/mp3/Pages.mp3'
 ];
 
 /* Start the service worker and cache all of the app's content */
